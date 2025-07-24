ba45708785dc51e20739d5adcd154e7ca33f6520
IUU6 <iuu6@duck.com>
2025-07-24 16:21:12 +0800

    Feat: Add jetbrains.idea-ultimate file

--- server/aura-latitude-5420/env/code-editor-env.nix
+++ server/aura-latitude-5420/env/code-editor-env.nix
@@ -10,5 +10,6 @@
     ./../../../code-editor-env/navicat-premium
     ./../../../code-editor-env/vscode
     ./../../../code-editor-env/github-desktop
+    ./../../../code-editor-env/jetbrain
   ];
 }
