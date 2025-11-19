586fc2847a027c153620d5a195e6a37256ff7ca8
IUU6 <iuu6@duck.com>
2025-11-20 00:33:39 +0800

    Feat: update and dbeaver

--- server/aura-latitude-5420/env/code-editor-env.nix
+++ server/aura-latitude-5420/env/code-editor-env.nix
@@ -8,6 +8,7 @@
 {
   imports = [
     # ./../../../code-editor-env/navicat-premium
+    ./../../../code-editor-env/dbeaver/default.nix
     ./../../../code-editor-env/vscode
     ./../../../code-editor-env/github-desktop
     ./../../../code-editor-env/jetbrain
