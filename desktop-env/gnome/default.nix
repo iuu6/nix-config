1a69b8312e5f7b4cad08043b4058793253d614a6
IUU6 <iuu6@duck.com>
2026-02-28 21:06:24 +0800

    Fix: buttom of minimize,maximize,close

--- desktop-env/gnome/default.nix
+++ desktop-env/gnome/default.nix
@@ -15,4 +15,19 @@
 
   # Enable fcitx5 autostart for GNOME
   services.desktopManager.gnome.sessionPath = [ config.i18n.inputMethod.package ];
+
+  # Install GNOME extensions for system tray support
+  environment.systemPackages = with pkgs; [
+    gnomeExtensions.appindicator
+    gnomeExtensions.tray-icons-reloaded
+  ];
+
+  # Fix window buttons layout to include minimize and maximize buttons
+  programs.dconf.profiles.user.databases = [{
+    settings = {
+      "org/gnome/desktop/wm/preferences" = {
+        button-layout = "appmenu:minimize,maximize,close";
+      };
+    };
+  }];
 }
