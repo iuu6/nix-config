bc745a8f1715b87479a9d125b3edf4a79e9f8f4b
IUU6 <iuu6@duck.com>
2026-06-14 15:23:18 +0800

    Feat: gnome ext

--- desktop-env/gnome/default.nix
+++ desktop-env/gnome/default.nix
@@ -16,18 +16,29 @@
   # Enable fcitx5 autostart for GNOME
   services.desktopManager.gnome.sessionPath = [ config.i18n.inputMethod.package ];
 
-  # Install GNOME extensions for system tray support
+  # Install GNOME extensions for system tray support and tiling window management
   environment.systemPackages = with pkgs; [
     gnomeExtensions.appindicator
     gnomeExtensions.tray-icons-reloaded
+    gnomeExtensions.paperwm
+    gnomeExtensions.system-monitor-next
   ];
 
   # Fix window buttons layout to include minimize and maximize buttons
+  # and enable AppIndicator/KStatusNotifierItem support extension
   programs.dconf.profiles.user.databases = [
     {
       settings = {
         "org/gnome/desktop/wm/preferences" = {
           button-layout = "appmenu:minimize,maximize,close";
+        };
+        "org/gnome/shell" = {
+          enabled-extensions = [
+            "appindicatorsupport@rgcjonas.gmail.com"
+            "trayIconsReloaded@selfmade.pl"
+            "paperwm@paperwm.github.com"
+            "system-monitor-next@paradoxxx.zero.gmail.com"
+          ];
         };
       };
     }
