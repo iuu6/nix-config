f8a993f86d31ea36b52695b517a94fbdcf4e2e98
IUU6 <iuu6@duck.com>
2026-07-30 22:15:01 +0800

    Feat: nvtop and top pstate

--- hardware-env/graphics/nouveau/default.nix
+++ hardware-env/graphics/nouveau/default.nix
@@ -17,5 +17,18 @@
 
   environment.systemPackages = with pkgs; [
     libva-utils
+    nvtopPackages.full
   ];
+
+  # Set nouveau GPU to max performance pstate at boot
+  systemd.services.nouveau-pstate = {
+    description = "Set Nouveau GPU pstate to maximum performance";
+    wantedBy = [ "multi-user.target" ];
+    after = [ "systemd-udev-settle.service" ];
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = true;
+      ExecStart = "${pkgs.bash}/bin/bash -c 'echo 0f > /sys/class/drm/card0/device/pstate'";
+    };
+  };
 }
