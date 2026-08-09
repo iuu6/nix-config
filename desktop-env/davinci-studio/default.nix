ed7754acab1f12302031e134ff926cf836ec93a0
IUU6 <iuu6@duck.com>
2026-08-09 16:35:48 +0800

    Fix: unfree-pkgs

--- desktop-env/davinci-studio/default.nix
+++ desktop-env/davinci-studio/default.nix
@@ -1,6 +1,13 @@
-{ inputs, ... }:
+{ inputs, lib, ... }:
+let
+  unfree-pkgs = import inputs.nixpkgs-unstable {
+    system = "x86_64-linux";
+    config.allowUnfreePredicate = pkg:
+      builtins.elem (lib.getName pkg) [ "davinci-resolve-studio" ];
+  };
+in
 {
   environment.systemPackages = [
-    inputs.nixpkgs-unstable.legacyPackages.x86_64-linux.davinci-resolve-studio
+    unfree-pkgs.davinci-resolve-studio
   ];
 }
